6e88f0e13f25816d3056861a5c70ac0a42f49eca
lucasmirandaa <lucasmirandaa458@gmail.com>
2022-05-22 17:23:50 -0300

    Script forms PHP

--- script.php
+++ script.php
@@ -8,7 +8,18 @@ $category[]='old';
 
 $name = $_POST['name'];//'Eobard Thawne';
 $age = $_POST['age'];
-
+if(empty($name)){
+    echo "The name can't be empty. ";
+}
+if(strlen($name) <3){
+    echo "Invalid name, too short. Minimum 3 characters. ";
+}
+if(strlen($name) >30){
+    echo "Name too long. ";
+}
+if(!is_numeric($age)){
+    echo "Only numbers. ";
+}
 if($age>=6 && $age<=12)
 {
     for($i = 0;$i <=count($category);$i++)
