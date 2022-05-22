78cb0ccc74c33c76db8a9a1e3cd385950caf5472
lucasmirandaa <lucasmirandaa458@gmail.com>
2022-05-22 14:23:09 -0300

    first form

--- index.php
+++ index.php
@@ -1,47 +1,14 @@
-<?php
-
-$category=[];
-$category[]='infantil';
-$category[]='adolescente';
-$category[]='adulto';
-$category[]='idoso';
-//print_r($category);
-
-$nome='Edu';
-$idade=19;
-
-//var_dump($nome);
-//var_dump($idade);
-
-if($idade>=6 && $idade<=12)
-{
-    for($i = 0;$i <=count($category);$i++)
-    {
-        if($category[$i]=='infantil')
-        echo "O nadador ".$nome." compete na categoria " .$category[$i];
-    }
-}
-elseif($idade>=13 && $idade<=18)
-{
-    for($i=0;$i<=count($category);$i++)
-    {
-        if($category[$i]=='adolescente')
-        echo "O nadador ".$nome." compete na categoria ".$category[$i];
-    }
-}
-elseif($idade>=19 && $idade<=59)
-{
-    for($i=0;$i<=count($category);$i++)
-    {
-        if($category[$i]=='adulto')
-        echo "O nadador ".$nome." compete na categoria ".$category[$i];
-    }
-}
-else
-{
-    for($i=0;$i<=count($category);$i++)
-    {
-        if($category[$i]=='idoso')
-        echo "O nadador ".$nome." compete na categoria ".$category[$i];
-    }
-}
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form PHP</title>
+</head>
+<body>
+    <p>Competitor Registration Form</p>
+    <form action="script.php" method="post">
+        <p>Name: <input type="text" name="nome"</p>
+    </form>
+</body>
+</html>
