cf5eb9b31171dc74006fea4a90272518cb9eb09d
lucasmirandaa <lucasmirandaa458@gmail.com>
2022-05-22 14:29:46 -0300

    First Form PHP

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="pt">
 <head>
     <meta charset="UTF-8">
+    <title>Form PHP</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form PHP</title>
 </head>
